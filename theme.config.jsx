e3bf55441d932e1e4af762a067d1d2eb1b211270
Justin Green <justingreendev@gmail.com>
2024-01-14 17:50:28 -0600

    JS content

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,37 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>Code Snippets</span>,
   project: {
     link: 'https://github.com/shuding/nextra'
-  }
+  },  
+  banner: {
+    key: '2.0-release',
+    text: (
+      <a href="https://nextra.site" target="_blank">
+        🎉 Nextra 2.0 is released. Read more →
+      </a>
+    )
+  },
+  themeSwitch: {
+    useOptions() {
+      return {
+        light: 'Light',
+        dark: 'Dark',
+        system: 'System'
+      }
+    }
+  },
+  editLink: {
+    text: 'Edit this page on GitHub'
+  },
+  toc: {
+    backToTop: true
+  },
+  // primaryHue: {
+  //   dark: 100,
+  //   light: 100,
+  // },
+  footer: {
+    text: `MIT ${new Date().getFullYear()} © DocsGPT`,
+  },
   // ... other theme options
 }
